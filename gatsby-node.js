9cfee8d327e7e44af21c7d7bab2a06387480d196
mayne <dreama9ain@gmail.com>
2019-09-09 19:34:28 +0800

    config from notion table

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,12 +3,31 @@ const genApiData = require('./src/genApiData')
 let nb = new Notabase()
 
 
-exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }, { dbMap, settings = {} }) => {
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }, options) => {
+
     const { createNode } = actions
-    let db = await nb.fetchAll(dbMap)
+    const { configTable, dbMap, settings } = options
+
+    let cacheSettings = {}
+    let _dbMap = {}
+    if (configTable) {
+        let config = await nb.fetch(configTable)
+        config.rows.filter(i => i).map(c => {
+            _dbMap[c.name] = c.table
+            if (c.isHtmlCache) {
+                cacheSettings[c.name] = 'html'
+            }
+        })
+    } else if (dbMap) {
+        _dbMap = dbMap
+    }
+    if (settings) {
+        cacheSettings = settings
+    }
+    let db = await nb.fetchAll(_dbMap)
     await Promise.all(Object.entries(db).map(async (i) => {
         let [tableName, collection] = i
-        await genApiData(nb, collection, tableName, 'id', createNode, createNodeId, createContentDigest, settings)
+        await genApiData(nb, collection, tableName, 'id', createNode, createNodeId, createContentDigest, cacheSettings)
     }))
     return
 }
